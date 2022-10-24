998a614f461e2c8da334f3be9abef845e90d5539
Agustin Ramos <agustinramos0504@gmail.com>
2022-10-24 19:42:08 -0300

    Primer commit, agregue estructura basica al html y declares constantes para los precios y variables para el stock

---
+++ script.js
@@ -0,0 +1,16 @@
+//Precio de los productos
+
+const iphone8 = 240
+const iphone8Plus = 300
+const iphoneXS = 350
+const iphone11 = 425
+const iphone12 = 560
+
+//Stock 
+
+let stockIphone8 = 2
+let stockIphone8Plus = 3
+let stockIphoneXS = 3
+let stockIphone11 = 5
+let stockIphone12 = 4
+
